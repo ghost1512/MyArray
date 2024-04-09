42e69405492028c78cb467fe744b3b1ba4ae01cd
BaoHG <baohuynh291@gmail.com>
2024-04-09 19:24:33 +0700

    add array
    array

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 3, 4 };
+        int sum = 0;
+        //index
+        // for (int i = 0; i < array.Length; i++)
+        // {
+        //     array[i] = i;
+        // }
+        for (int i = 0; i < array.Length; i++)
+        {
+            sum = sum + array[i];
+        }
+        Console.WriteLine($"Tong la {sum}");
+    }
+}
